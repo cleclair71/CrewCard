5e2b7073cd33e35cc6a9975d711de9cd652f2028
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-06 12:25:28 -0500

    adding template to index.js

--- index.js
+++ index.js
@@ -170,27 +170,224 @@ async function getAvatarUrl(username) {
     return userData.avatar_url;
 }
 
-const writeFile = data => {
-    fs.writeFile('./dist/index.html', pageHTML, {encoding:'utf8',flag:'w'}, data, err => {
-        if (err) {
+function addAnotherEmployee() {
+    inquirer
+        .prompt(
+            {
+                type: 'list',
+                message: 'Would you like to add another employee?',
+                choices: ['Add Engineer', 'Add Intern', 'Crew is Complete!'],
+                name: 'action',
+            }
+        )
+        .then((response) => {
+
+            if (response.action === 'Crew is Complete!') {
+                writeToFile();
+            } 
+
+            else {
+                if (response.action === 'Add Engineer') {
+                    var inquirerPrompt = generateEngineer; 
+                }
+                else if (response.action === 'Add Intern') {
+                    var inquirerPrompt = generateIntern;
+                }
+                inquirer
+                    .prompt(inquirerPrompt)
+                    .then((response) => {
+                        addRole(response);
+                        addAnotherEmployee();
+                    })
+            }
+        })
+}
+
+function writeToFile() {
+    var data = generateHTML();
+    fs.writeFile('./dist/index.html', data, (err) => {
+        if (err)
             console.log(err);
-            return;
-        } else {
-            console.log('Your Crew Cards have been created! Check out index.html to see it!');
+        else {
+            console.log("Your Crew Cards have been generated! Check them out in './dist/index.html'");
         }
     });
-};
-addManager()
-    .then(addEmployee)
-    .then(crewArray => {
-        return generateCrew(crewArray);
+}
+
+function generateHTML() {
+    var template = 
+    `
+    <!Doctype html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Crew Card</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Concert+One&family=Montserrat+Alternates:wght@100;300&display=swap"
+            rel="stylesheet">
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <header class="header sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+                <div class="container"><a class="navbar-brand" href="#">
+                        <a href="#" class="navbar-brand"><img src="../assets/cc.png"></a>
+                        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation"><span
+                                class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active"><a href="#" onclick="alert('Please open your terminal and enter the command: cd \CrewCard> node index.js')">Make New Crew</a></li>
+                            </ul>
+                        </div>
+                </div>
+            </nav>
+        </header>
+    
+        <!-- team cards -->
+        <div class="profile-area">
+            <div class="jumbotron">
+                <h1 class="display-4">Your Crew</h1>
+                <p class="lead">Teamwork is
+                    the backbone of any successful project.</p>
+                <hr class="my-4">
+                <div class="container">
+                    <div class="row justify-content-center">
+
+                    ${generateCrew()}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.2);">
+            The MIT Licence (MIT) 2023:
+            <a class="text-info" href="https://github.com/cleclair71?tab=repositories">Cassie LeClair</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>`;
+return template;
+}
+
+function generateCrew() {
+    var crewCard = "";
+    employees.forEach(employee => {
+        switch (employee.constructor.name) {
+            case 'Manager':
+                crewCard += 
+                `
+                <div class="col-md-4">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <div class="img1"><img src="../assets/managerpic.jpg" alt=""></div>
+                                            <div class="avatar"><img src="${employee.avatar_url}" onerror="this.onerror=null;this.src='../assets/defaultprofile.jpg';" alt="${employee.username}"></div>
+                                            <div class="main-text">
+                                                    <h3>${employee.name}</h3>
+                                                    <h4>Manager</h4><img class="logo" src="../assets/managericon.png"
+                                                        alt="manager icon"> <br>
+                                                    <a class="btn btn-primary" data-toggle="collapse" href="#collapseManager"
+                                                        role="button" aria-expanded="false" aria-controls="collapseManager">
+                                                        @
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <div class="card-body collapse" id="collapseManager">
+                                                <p class="id">ID: ${employee.id}</p>
+                                                <p class="email">Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+                                                <p class="office">Office Number: ${employee.officeNumber}
+                                                </p>
+                                            </div>
+                
+                                    </div>
+                                </div>
+                `;
+                break;
+            case 'Engineer':
+                crewCard += 
+                `
+                <div class="col-md-4">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <div class="img1"><img src="../assets/engineerpic.jpg" alt=""></div>
+                                            <div class="avatar"><img src="${employee.avatar_url}" onerror="this.onerror=null;this.src='../assets/defaultprofile.jpg';" alt="${employee.username}"></div>
+                                            <div class="main-text">
+                                                    <h3>${employee.name}</h3>
+                                                    <h4>Engineer</h4><img class="logo" src="../assets/developer.png"
+                                                        alt="engineer icon">
+                                                    <br>
+                                                    <a class="btn btn-primary" data-toggle="collapse" href="#collapseEngineer"
+                                                        role="button" aria-expanded="false" aria-controls="collapseEngineer"> @ </a>
+                
+                                                </div>
+                                            </div>
+                                            <div class="card-body collapse" id="collapseEngineer">
+                                                <p class="id">ID: ${employee.id}</p>
+                                                <p class="email">Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+                                                <p class="github">Github: <a
+                                                        href="https://github.com/${employee.github}">${employee.github}</a></p>
+                                            </div>
+                                    </div>
+                                </div>
+                `;
+                break;
+            case 'Intern':
+                crewCard += 
+                `
+                <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                 <div class="img1"><img src="../assets/internpic.jpg" alt=""></div>
+                        <div class="avatar"><img src="${employee.avatar_url}" onerror="this.onerror=null;this.src='../assets/defaultprofile.jpg';" alt="${employee.username}"></div>
+                        <div class="main-text">
+                     <h3>${employee.name}</h3>
+                        <h4>Intern</h4><img class="logo" src="../assets/internship.png" alt="intern icon">
+                         <br>
+                        <a class="btn btn-primary" data-toggle="collapse" href="#collapseIntern" role="button" aria-expanded="false" aria-controls="collapseIntern"> @</a>
+                     </div>
+                                        </div>
+                                        <div class="card-body collapse" id="collapseIntern">
+                                            <p class="id">ID: ${employee.id}</p>
+                                            <p class="email">Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+                                            <p class="school">School: ${employee.school}</p>
+                                        </div>
+                </div>
+            </div>
+            `;
+                break;
+        }
     })
-    .then(pageHTML => {
-        return fs.writeFileSync('./dist/index.html', pageHTML, {encoding:'utf8',flag:'w'});     
-    })
-    .then(() => {
-        console.log('Your Crew Cards have been created! Check out index.html to see it!');
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    return crewCard;
+}
+
+function init() {
+    inquirer
+        .prompt(addManager)
+        .then((response) => {
+            addRole(response);
+            addAnotherEmployee(); 
+        })
+}
+
+init();
